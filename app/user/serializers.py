c1631401e1f7e8824935713deca5e9cb6f4a0a3d
Hannah <hannahvanseeters@gmail.com>
2019-01-09 12:13:51 -0800

    added other categories to user form endpoint when create it

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -9,7 +9,10 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'password', 'company_name')
+        fields = (
+            'email', 'password', 'company_name', 'point_of_contact', 'is_doner',
+            'street_address', 'street_address2', 'city', 'state', 'zip'
+        )
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
